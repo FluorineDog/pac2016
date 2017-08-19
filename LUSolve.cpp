a16f2368320521bfed004f01354d3aeb592333dd
fluorinedog <fluorinedog@gmail.com>
2017-08-19 21:23:02 +0800

    add debugger

--- LUSolve.cpp
+++ LUSolve.cpp
@@ -1,8 +1,37 @@
 #include "LUSolve.h"
-#include <iostream> 
+#include <algorithm>
+#include <iostream>
+#include <memory>
 using std::cout;
 using std::cerr;
 using std::endl;
+void dog_checker(const SprsMatRealStru &A, const double x[],
+                 const double std_b[]) {
+  auto a = A.pdVal;
+  auto ia = A.Mat.piIstart;
+	auto ja = A.Mat.piJno;
+	auto n = A.Mat.iDim + 1;
+	std::unique_ptr<double[]> b(new double[n]);
+  for (int i = 0; i < n; ++i) {
+    double sum = 0;
+    for (int jLoc = ia[i]; jLoc < ia[i + 1]; ++jLoc) {
+			int j = ja[jLoc];
+      b[i] += x[j]*a[jLoc];
+      if(j != i){
+        b[j] += x[i]*a[jLoc];
+      }
+		}	
+  }
+  double maxdiff = 0;
+  for(int i = 0; i < n; ++i){
+    b[i] -= std_b[i];
+    maxdiff = std::max(b[i], maxdiff);
+  }
+  cerr << "maxdiff: " << maxdiff << endl;
+  auto count = std::count_if(b.get(), b.get() + n, [=](double x){return x > maxdiff/10;});
+  cerr << " and " << count << " numbers is in the same magnitude" << endl;
+}
+							
 
 int main(int argc, const char *argv[]) {
   unsigned long long timeStart; // 仿真开始时间
@@ -18,30 +47,18 @@ int main(int argc, const char *argv[]) {
 
   initMem_MatReal(&A);
   initMem_UMatReal(&U);
-  // add by wsh
 
-  // add end by wsh 20170621
   //添加构建A阵的读写函数
   printf("Begin ReadMatrixA...\n");
   ReadMatrixA(&A, "A.txt");
   printf("ReadMatrixA finish!\n");
   ////////////////////////
 
-  //测试写AA////////////////
-  // WriteMatrixA(&A,"AA.txt");
-  //////////////////////////
   nsize = 10000;
-  //  B = new VecRealStru[nsize];
   printf("Begin ReadVectorB...\n");
   //添加构建B向量的函数
   ReadVectorB(&B, nsize, "B.txt");
   printf("ReadVectorB finish!\n");
-  ////////////////////////
-  // printf("%22.15e\n", B[1].pdVal[1]);
-  // for (i = 0; i < nsize;i++)
-  // WriteVectorB(&B[i], nsize, "BB.txt");
-
-  //   WriteVectorX(B, nsize, "X.txt");
   //初始化X数组
   X = new VecRealStru[nsize];
   for (i = 0; i < nsize; i++) {
@@ -54,7 +71,6 @@ int main(int argc, const char *argv[]) {
 
   printf("The Program is Running...\n");
   // 获取仿真开始时间
-  //	timeStart = clock();//modify by wsh 2017.7.31
   timeStart = rdtsc();
   {
     LU_SymbolicSymG(&A, &U);
@@ -68,7 +84,6 @@ int main(int argc, const char *argv[]) {
     }
 
     // 获取仿真结束时间
-    //	timeEnd = clock();  //modify by wsh 2017.7.31
   }
   timeEnd = rdtsc();
   delapseTime = (double)(timeEnd - timeStart) / (F * Time);
@@ -76,8 +91,8 @@ int main(int argc, const char *argv[]) {
   printf("The program elapsed %13.8f s\n", delapseTime);
 
   //添加打印结果相量的代码
-  printf("Begin Print Result...(enabled)\n");
-  WriteVectorX(X, nsize, "X1.txt");
+  printf("Begin Print Result...(disabled)\n");
+  // WriteVectorX(X, nsize, "X1.txt");
   printf("Print Result finish!\n");
   ////////////////////////
   //初始化X数组
@@ -89,6 +104,12 @@ int main(int argc, const char *argv[]) {
   }
 
   CompareVectorX(Reference, nsize, X);
+  cerr << "intel trash" << endl;
+  dog_checker(A, X[0].pdVal, B[0].pdVal);
+  cerr << "--------------------------" << endl;
+  cerr << "std trash" << endl;
+  dog_checker(A, Reference[0].pdVal, B[0].pdVal);
+
   deallocate_MatReal(&A);
   deallocate_UMatReal(&U);
   for (i = 0; i < nsize; i++) {
